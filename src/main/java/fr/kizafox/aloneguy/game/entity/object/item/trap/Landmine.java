90b3c1bb0106cd676145e5cbbb61f010e589b102
KIZAFOX <victor.pernak@hotmail.fr>
2024-01-07 14:32:58 +0100

    :rocket: Landmine added

---
+++ src/main/java/fr/kizafox/aloneguy/game/entity/object/item/trap/Landmine.java
@@ -0,0 +1,18 @@
+package fr.kizafox.aloneguy.game.entity.object.item.trap;
+
+import fr.kizafox.aloneguy.game.client.window.Game;
+import fr.kizafox.aloneguy.game.entity.Entity;
+import fr.kizafox.aloneguy.game.utils.ImageRenderer;
+import fr.kizafox.aloneguy.game.utils.sound.SoundPlayer;
+
+public class Landmine extends Trap {
+    public Landmine(Game game) {
+        super(game, "Landmine", 27, 21, ImageRenderer.load(ImageRenderer.LANDMINE));
+    }
+
+    @Override
+    public void applyEffect(Entity player) {
+        player.setHealth(player.getHealth() - 5);
+        SoundPlayer.playSound(SoundPlayer.EXPLOSION_SOUND, SoundPlayer.Volume.MEDIUM);
+    }
+}
